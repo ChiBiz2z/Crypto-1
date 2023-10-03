adc890c92b3934546f594d1d746941e78f4a4903
Vladimir <vvbedash@gmail.com>
2023-10-03 11:09:26 +0600

    mono refactor

--- lab1/Encryption/MonoEncoder.cs
+++ lab1/Encryption/MonoEncoder.cs
@@ -1,24 +1,25 @@
-﻿using lab1.Encryption;
+﻿using lab1;
+using lab1.Encryption;
+using System.Linq;
 using System.Text;
-using System;
-using lab1;
 
 public class MonoEncoder : ICryptable
 {
+    private readonly string ReverseAlphabet = new string(AppConstants.RussianAlphabetWithLowerCase.Reverse().ToArray());
 
     public string Encrypt(string inputText)
-        => Encode(inputText, (index) => AppConstants.RussianAlphabetWithLowerCase.Length - index - 1);
+        => Encode(inputText, AppConstants.RussianAlphabetWithLowerCase, ReverseAlphabet);
 
     public string Decrypt(string inputText)
-        => Encode(inputText, (index) => AppConstants.RussianAlphabetWithLowerCase.Length - index - 1);
+        => Encode(inputText, ReverseAlphabet, AppConstants.RussianAlphabetWithLowerCase);
 
-    private string Encode(string text, Func<int, int> operation)
+    private string Encode(string text, string fromAlphabet, string toAlphabet)
     {
         var sb = new StringBuilder();
         foreach (char c in text)
         {
-            var index = AppConstants.RussianAlphabetWithLowerCase.IndexOf(c);
-            sb.Append(index < 0 ? c : AppConstants.RussianAlphabetWithLowerCase[operation(index)]);
+            var index = fromAlphabet.IndexOf(c);
+            sb.Append(index < 0 ? c : toAlphabet[index]);
         }
 
         return sb.ToString();
